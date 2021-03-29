d6185c219482a1b3e41bffc376cbbc8ec7427b95
Mitchel van Eijgen <mvaneijgen@gmail.com>
2021-03-29 16:16:18 +0200

    add numsym, ctrl and game layer

--- config.h
+++ config.h
@@ -247,3 +247,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 180
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
+// #define COMBO_COUNT 5
