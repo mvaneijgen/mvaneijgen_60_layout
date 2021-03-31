f4d590861f7483d3f63f65ad76dde3fbb9b83ce5
Mitchel van Eijgen <mvaneijgen@gmail.com>
2021-03-31 11:43:02 +0200

    add multi layer home row mods

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -20,41 +20,42 @@
 #define _GAME 3
 #define _RESET 4
 
-//------------------------------------------------------//
-// 🏠️ Home row mods 
-//------------------------------------------------------//
-// Left-hand home row mods
-#define CTL_A LCTL_T(KC_A)
-#define ALT_S LALT_T(KC_S)
-#define GUI_D LGUI_T(KC_D)
-#define SFT_F LSFT_T(KC_F)
+// //------------------------------------------------------//
+// // 🏠️ Home row mods 
+// //------------------------------------------------------//
+// // _BASE
+// // Left-hand home row mods
+// #define CTL_A LCTL_T(KC_A)
+// #define ALT_S LALT_T(KC_S)
+// #define GUI_D LGUI_T(KC_D)
+// #define SFT_F LSFT_T(KC_F)
 
-// Right-hand home row mods
-#define SFT_J LSFT_T(KC_J)
-#define GUI_K LGUI_T(KC_K)
-#define ALT_L LALT_T(KC_L)
-#define CTL_BSLS LCTL_T(KC_BSLS)
-// END 🏠️ Home row mods -------------------------------------//
+// // Right-hand home row mods
+// #define SFT_J RSFT_T(KC_J)
+// #define GUI_K RGUI_T(KC_K)
+// #define ALT_L LALT_T(KC_L)
+// #define CTL_BSLS RCTL_T(KC_BSLS)
+// // END 🏠️ Home row mods -------------------------------------//
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
 		LT(_RESET, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_MUTE, 
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT, KC_MPLY, 
-		KC_CAPS, CTL_A, ALT_S, GUI_D, SFT_F, KC_G, KC_H, SFT_J, GUI_K, ALT_L, KC_SCLN, KC_QUOT, KC_BSLS, KC__VOLUP, 
-		KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, KC_UP, KC__VOLDOWN, 
+		KC_CAPS, LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RGUI_T(KC_K), LALT_T(KC_L), RCTL_T(KC_BSLS), KC_QUOT, KC_SCLN, KC_VOLU, 
+		KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_DOT, KC_COMMA, KC_SLSH, KC_DEL, KC_UP, KC_VOLD, 
 		KC_LCTL, KC_LALT, MO(_NUMSYM), KC_SPC, MO(_CTRL), KC_NO, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_NUMSYM] = LAYOUT(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_ENT, KC_MPLY, 
-		KC_TRNS, KC_LT, KC_LCBR, KC_LPRN, KC_LBRC, KC_QUOT, KC_DQT, KC_RBRC, KC_RPRN, KC_RCBR, KC_GT, KC_QUOT, KC_BSLS, KC__VOLUP, 
-		KC_TRNS, KC_TILD, KC_UNDS, KC_MINS, KC_SLSH, KC_COLN, KC_SLSH, KC_SCLN, KC_BSLS, KC_PLUS, KC_COMMA, KC_DOT, KC_DEL, KC_UP, KC__VOLDOWN, 
+		KC_TRNS, LCTL_T(KC_LT), LALT_T(KC_LCBR), LGUI_T(KC_LPRN), LSFT_T(KC_LBRC), KC_QUOT, KC_DQT, RSFT_T(KC_RBRC), LALT_T(KC_RPRN), LALT_T(KC_RCBR), RCTL_T(KC_GT), KC_QUOT, KC_BSLS, KC_VOLU, 
+		KC_TRNS, KC_TILD, KC_UNDS, KC_MINS, KC_SLSH, KC_COLN, KC_SLSH, KC_SCLN, KC_BSLS, KC_PLUS, KC_COMMA, KC_DOT, KC_DEL, KC_UP, KC_VOLD, 
 		KC_TRNS, KC_TRNS, TG(_GAME), KC_ENT, TG(_GAME), MO(_RESET), KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_CTRL] = LAYOUT(
 		RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_MFFD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BRMD, KC_BRMU, KC__VOLDOWN, KC_MPLY, KC__VOLUP, KC__MUTE, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, LALT_T(KC_LEFT), LGUI_T(KC_DOWN), LSFT_T(KC_RGHT), KC_BRMD, KC_BRMU, RSFT_T(KC_VOLD), RGUI_T(KC_MPLY), LALT_T(KC_VOLU), RCTL_T(KC__MUTE), KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MRWD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
